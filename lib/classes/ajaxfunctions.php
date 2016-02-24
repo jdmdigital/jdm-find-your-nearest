aea666dfdb05a20545f1d5eb3fac11cd5b5600ce
JDM Digital <nerds@jdmdigital.co>
2016-02-24 13:24:22 -0600

    Fix Issue #6

--- lib/classes/ajaxfunctions.php
+++ lib/classes/ajaxfunctions.php
@@ -8,19 +8,21 @@ class AjaxFunctions{
         $Display_Results=$options['Display_Results'];
         $lat1=$_POST['lat'];
         $lng1=$_POST['lng'];
-        $args=array(
+        
+		$args=array(
                     'post_type' => 'find_your_nearest',
                     'posts_per_page' => '-1'
                 );
         // The Query
         $the_query = new WP_Query( $args );
-
+		
         // build array from The Loop
         $results=array();
+
 		while ( $the_query->have_posts() ) : $the_query->the_post();
 			$lng2=get_post_meta (get_the_ID(), '_aphs_FYN_longitude', TRUE );
             $lat2=get_post_meta (get_the_ID(), '_aphs_FYN_latitude', TRUE );
-            //calculate distance from
+			//calculate distance from
             if (is_numeric($lng2) && is_numeric($lat2)) {
                 $theta = $lng1 - $lng2;
                 $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
@@ -38,14 +40,14 @@ class AjaxFunctions{
                 $content = str_replace('><br />', '>', $content);
                 $title=get_the_title();
 				$link = get_permalink();
-				
+				$serviceclasses = getall_service_classes();
 				switch ($Distance_Units) {
 					//case "miles":$results["$miles"][$title]="<div class='col-md-4 col-sm-6'><div class='card locations-card'><div class='card-body'><h3 class='card-title'><a href='$link'>$title</a></h3><span class='card-text'><em>Distance ".round($miles)." miles</em> <span class='FYN_viewmap' style='cursor: pointer' id='".get_the_ID()."'>view map</span><p>".$content."</p></span></div><div class='card-footer'><a class='btn btn-link' href='$link'>Details</a><a class='btn btn-link-success' href='tel:".the_fyn_phone()."'>Call: ".the_fyn_phone()."</a></div></div></div>";
-					case "miles":$results["$miles"][$title]="<div class='col-md-4 col-sm-6 ".getall_service_classes()."'><div class='card locations-card'><div class='card-body'><h3 class='card-title'><a href='".$link."'>".$title."</a></h3><span class='card-text'><p><em>Distance ".round($miles)." miles</em><br />".$content."</p></span></div><div class='card-footer service-icons'>".getall_service_icons()."</div><div class='card-footer'><a class='btn margin-left btn-info' href='".$link."'>View Details</a><!--<a class='btn btn-link-default' href='tel:".the_fyn_phone()."'>".the_fyn_phone()."</a>--><!--<a class='btn margin-left btn-success btn4-hauling' href='".get_permalink(1070)."?loc=".urlencode($title)."'>Request Service</a>--></div></div></div>";
+					case "miles":$results["$miles"][$title]="<div class='col-md-4 col-sm-6 location-display".$serviceclasses."'><div class='card locations-card'><div class='card-body'><h3 class='card-title'><a href='".$link."'>".$title."</a></h3><span class='card-text'><p><em>Distance ".round($miles)." miles</em><br />".$content."</p></span></div><div class='card-footer service-icons'>".getall_service_icons()."</div><div class='card-footer'><a class='btn margin-left btn-info' href='".$link."'>View Details</a><!--<a class='btn btn-link-default' href='tel:".the_fyn_phone()."'>".the_fyn_phone()."</a>--><!--<a class='btn margin-left btn-success btn4-hauling' href='".get_permalink(1070)."?loc=".urlencode($title)."'>Request Service</a>--></div></div></div>";
 					break;
-					case "kilometres":$results["$miles"][$title]="<div class='col-md-4 col-sm-6 ".getall_service_classes()."'><div class='card locations-card'><div class='card-body'><h3 class='card-title'>".$title."</h3><span class='card-text'><em>Distance ".round($miles * 1.609344)."km</em> <span class='FYN_viewmap' style='cursor: pointer' id='".get_the_ID()."'>view map</span><p>".$content."</p></span></div><div class='card-footer'><a class='btn btn-link' href='".$link."'>View Details</a></div></div></div>";
+					case "kilometres":$results["$miles"][$title]="<div class='col-md-4 col-sm-6 ".$serviceclasses."'><div class='card locations-card'><div class='card-body'><h3 class='card-title'>".$title."</h3><span class='card-text'><em>Distance ".round($miles * 1.609344)."km</em> <span class='FYN_viewmap' style='cursor: pointer' id='".get_the_ID()."'>view map</span><p>".$content."</p></span></div><div class='card-footer'><a class='btn btn-link' href='".$link."'>View Details</a></div></div></div>";
 					break;
-					default:$results["$miles"][$title]="<div class='col-md-4 col-sm-6 ".getall_service_classes()."'><div class='card locations-card'><div class='card-body'><h3 class='card-title'><a href='".$link."'>".$title."</a></h3><span class='card-text'><em>Distance ".round($miles)." miles</em> <span class='FYN_viewmap' style='cursor: pointer' id='".get_the_ID()."'>view map</span><p>".$content."</p></span></div><div class='card-footer'><a class='btn btn-link' href='".$link."'>View Details</a></div></div></div>";
+					default:$results["$miles"][$title]="<div class='col-md-4 col-sm-6 ".$serviceclasses."'><div class='card locations-card'><div class='card-body'><h3 class='card-title'><a href='".$link."'>".$title."</a></h3><span class='card-text'><em>Distance ".round($miles)." miles</em> <span class='FYN_viewmap' style='cursor: pointer' id='".get_the_ID()."'>view map</span><p>".$content."</p></span></div><div class='card-footer'><a class='btn btn-link' href='".$link."'>View Details</a></div></div></div>";
 					break;
 				}
 				
@@ -59,10 +61,18 @@ class AjaxFunctions{
             if ($Display_Results!=0 AND $Display_Results<count($results)) {
                 $results = array_slice($results, 0, $Display_Results);
             }
+			$i = 0;
             foreach ($results as $distance=>$content) {
                 ksort($content);
                 foreach ($content as $item) {
-                    echo $item;
+                    if(strpos($item, 'hauling-loc') !== false && $i == 0) {
+						echo '<div class="location-item show">';
+						$i++;
+					} else {
+						echo '<div class="location-item">';
+					}
+					echo $item;
+					echo '</div>';
                 }
             }
         } else {
